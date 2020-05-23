f74d0680edaf77be9cf0c80f34ed92816b06e045
AnatoliiMikhov <anatoli.mihov@gmail.com>
2020-05-23 14:51:38 +0300

    tabs script code Done

--- js/script.js
+++ js/script.js
@@ -1,2 +1,51 @@
 'use strict';
 
+document.addEventListener('DOMContentLoaded', () => {
+
+    const tabsParent = document.querySelector('.tabcontainer'),
+        tabs = tabsParent.querySelectorAll('.tabheader__item'),
+        tabsContent = tabsParent.querySelectorAll('.tabcontent');
+
+
+    /* ------------------------------ function calls ----------------------------- */
+// Tabs
+    hideTabContent();
+    showTabContent();
+
+    /* -------------------------- function declarations -------------------------- */
+// Tabs
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    /* --------------------------------- Events --------------------------------- */
+
+// Tabs
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+
+
+});
