ce73a245b6f5d991f4f6753dd5d5ca1608493469
AnatoliiMikhov <anatoli.mihov@gmail.com>
2020-05-27 12:32:50 +0300

    send form data format multipart/form-data

--- js/script.js
+++ js/script.js
@@ -228,5 +228,53 @@ document.addEventListener('DOMContentLoaded', () => {
 		`.menu .container`
 	).render();
 
+	/* ----------------------------- Send Form Data ----------------------------- */
+	// Forms
+
+	const forms = document.querySelectorAll('form');
+
+	const message = {
+		loading: 'Loading',
+		success: 'Thank you we will call you',
+		failure: 'Something went wrong'
+	};
+
+	forms.forEach(item => {
+		postData(item);
+	});
+
+	function postData(form) {
+		form.addEventListener('submit', (event) => {
+			event.preventDefault();
+
+			const statusMessage = document.createElement('div');
+			statusMessage.classList.add('status');
+			statusMessage.textContent = message.loading;
+			form.append(statusMessage);
+
+			const request = new XMLHttpRequest();
+			request.open('POST', 'server.php');
+
+			// request.setRequestHeader('Content-type', 'multipart/form-data');
+			const formData = new FormData(form);
+
+			request.send(formData);
+
+			request.addEventListener('load', () => {
+				if (request.status === 200) {
+					console.log(request.response);
+					statusMessage.textContent = message.success;
+					form.reset();
+					setTimeout(() => {
+						statusMessage.remove();
+					}, 2000);
+				} else {
+					statusMessage.textContent = message.failure;
+				}
+			});
+
+		});
+	}
+
 
 });
