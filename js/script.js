60afee5128eaf0e9cc7d8ddc3cc60531d05ef0a1
AnatoliiMikhov <anatoli.mihov@gmail.com>
2020-05-28 22:16:44 +0300

    Axios usage

--- js/script.js
+++ js/script.js
@@ -150,7 +150,8 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 	}
 
-	const getResouce = async (url) => {
+	// fetch usage
+	/* const getResouce = async (url) => {
 		const res = await fetch(url);
 
 		if (!res.ok) {
@@ -158,37 +159,25 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 
 		return await res.json();
-	};
-	// version 1
-	getResouce('http://localhost:3000/menu')
+	}; */
+
+	/* getResouce('http://localhost:3000/menu')
 		.then(data => {
 			data.forEach(({ img, altimg, title, descr, price }) => {
 				new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
 			});
-		});
-	// version 2
-	/* getResouce('http://localhost:3000/menu')
-		.then(data => createCard(data));
+		}); */
 
-	function createCard(data) {
-		data.forEach(({ img, altimg, title, descr, price }) => {
-			const element = document.createElement('div');
-			element.classList.add('menu__item');
-			element.innerHTML = `
-				<img src=${img} alt=${altimg} />
-				<h3 class="menu__item-subtitle">${title}</h3>
-				<div class="menu__item-descr">
-					${descr}
-				</div>
-				<div class="menu__item-divider"></div>
-				<div class="menu__item-price">
-				<div class="menu__item-cost">Цена:</div>
-				<div class="menu__item-total"><span>${price}</span> грн/день</div>
-				</div>
-			`;
-			document.querySelector('.menu .container').append(element);
+	// axios usage library
+	axios.get('http://localhost:3000/menu')
+		.then(data => {
+			data.data.forEach(({ img, altimg, title, descr, price }) => {
+				new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+			});
 		});
-	} */
+
+
+
 
 	/* ---------------------------------- Modal --------------------------------- */
 	// Modal
